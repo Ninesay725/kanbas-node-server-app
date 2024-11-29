f0ac3eaade3885e079605e31eded60860f08ec5c
Ninesay <xul28@uci.edu>
2024-11-29 00:26:48 -0800

    test5

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+        origin: process.env.NETLIFY_URL,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
